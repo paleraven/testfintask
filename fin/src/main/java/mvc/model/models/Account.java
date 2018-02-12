aeef584e326bfe593fda58d9678c6885f7b121da
Kretov <1357999>
2018-02-12 17:52:49 +0300

    added model (account, balance, incomeReport)

---
+++ fin/src/main/java/mvc/model/models/Account.java
@@ -0,0 +1,39 @@
+package mvc.model.models;
+
+import java.util.Date;
+
+/**
+ * Information about organization
+ */
+public class Account {
+    private int id;
+    private String name;
+    private Date created;
+
+    public Account() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
