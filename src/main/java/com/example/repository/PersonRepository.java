1c78807dfe2b05ba086f42d0ad9d4839094a92dd
SlavaLymar <slavalymar@gmail.com>
2018-02-13 15:23:35 +0300

    one more commit

--- src/main/java/com/example/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.domain.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
