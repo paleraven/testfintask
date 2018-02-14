d1db8764d558aedab4eabc04e89cb8bd2a741ccd
Kretov <1357999>
2018-02-14 18:27:14 +0300

    Added validation xml by xsd (for Balance and IncomeReport)

---
+++ src/main/java/sample/data/jpa/domain/FormType.java
@@ -0,0 +1,6 @@
+package sample.data.jpa.domain;
+
+public enum FormType {
+    BALANCE,
+    INCOME_REPORT
+}
---
+++ src/main/java/sample/data/jpa/util/XmlValidate.java
@@ -0,0 +1,53 @@
+package sample.data.jpa.util;
+
+import java.io.File;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.xml.sax.SAXException;
+import sample.data.jpa.domain.FormType;
+
+public class XmlValidate {
+
+    private XmlValidate() {
+    }
+
+    public static boolean validateXmlByXsd(FormType formType, String pathXml)
+            throws Exception {
+
+        try {
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            String pathXsd;
+
+            switch (formType) {
+                case BALANCE:
+                    pathXsd = classLoader.getResource("xsd/balance.xsd").toString();
+                    break;
+                case INCOME_REPORT:
+                    pathXsd = classLoader.getResource("xsd/income_report.xsd").toString();
+                    break;
+                default:
+                    return false;
+            }
+
+            File xml = new File(pathXml);
+
+            if (!xml.exists()) {
+                return false;
+            }
+
+            SchemaFactory factory = SchemaFactory
+                    .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new StreamSource(pathXsd));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(pathXml));
+            return true;
+        } catch (SAXException e) {
+            return false;
+        }
+
+    }
+
+}
