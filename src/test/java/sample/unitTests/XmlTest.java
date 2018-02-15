17506e796469f00bca406bbbfd3129c683e35083
VErmakova <ermakovavo@gmail.com>
2018-02-15 18:17:25 +0300

    xsd

--- src/test/java/sample/unitTests/XmlTest.java
+++ src/test/java/sample/unitTests/XmlTest.java
@@ -1,14 +1,13 @@
 package sample.unitTests;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import sample.data.jpa.domain.Balance;
 import sample.data.jpa.domain.IncomeReport;
 import sample.data.jpa.service.ToXMLServiceImpl;
 
 import java.math.BigDecimal;
-import java.util.Date;
+
 
 public class XmlTest {
 
@@ -16,40 +15,44 @@ public class XmlTest {
     public void ObjToXMLBalance() {
         Balance balance = new Balance();
 
+        balance.setBalanceId(12L);
+
         balance.setVersion(1);
-        balance.setCreated(new Date());
         balance.setStatus("Processing");
+        balance.setYear("2000");
         balance.setRow1300(new BigDecimal(12));
         balance.setRow1700(new BigDecimal(12));
 
-        ToXMLServiceImpl.commonToXML(balance, "");
+        ToXMLServiceImpl.commonToXML(balance, "1.xml");
     }
 
-    @Ignore
+
     @Test
     public void xmlToObjBalance() {
 
-        Balance balance = ToXMLServiceImpl.jaxbXMLToObjectBalance("");
-        Assert.assertEquals("Processing",balance.getStatus());
+        Balance balance = ToXMLServiceImpl.jaxbXMLToObjectBalance("1.xml");
+        Assert.assertEquals("Processing", balance.getStatus());
     }
 
     @Test
     public void ObjToXMLIncomeRep() {
         IncomeReport incomeReport = new IncomeReport();
 
+        incomeReport.setReportId(12l);
         incomeReport.setVersion(1);
-        incomeReport.setCreated(new Date());
+
         incomeReport.setStatus("Processing");
+        incomeReport.setYear("2012");
         incomeReport.setRow2110(new BigDecimal(12));
         incomeReport.setRow2210(new BigDecimal(12));
 
-        ToXMLServiceImpl.commonToXML(incomeReport, "");
+        ToXMLServiceImpl.commonToXML(incomeReport, "2.xml");
     }
 
     @Test
     public void xmlToObjIncomeRep() {
 
-        IncomeReport incomeReport = ToXMLServiceImpl.jaxbXMLToObjectIM("");
-        Assert.assertEquals("Processing",incomeReport.getStatus());
+        IncomeReport incomeReport = ToXMLServiceImpl.jaxbXMLToObjectIM("2.xml");
+        Assert.assertEquals("Processing", incomeReport.getStatus());
     }
 }
