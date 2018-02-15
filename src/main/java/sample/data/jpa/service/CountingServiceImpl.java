a14a4bec9f6011e025c2c7f464825a7cbbe02693
VErmakova <ermakovavo@gmail.com>
2018-02-15 13:46:37 +0300

    Balance rules rdy

--- src/main/java/sample/data/jpa/service/CountingServiceImpl.java
+++ src/main/java/sample/data/jpa/service/CountingServiceImpl.java
@@ -5,95 +5,110 @@ import sample.data.jpa.domain.Balance;
 
 import java.math.BigDecimal;
 
-public class CountingServiceImpl extends Balance implements CountingService {
+import sample.data.jpa.util.Utils;
+
+public class CountingServiceImpl implements CountingService {
 
     private String message = "Data inconsistency";
 
     private Balance balance;
-    private CountingServiceImpl countingService;
 
     public CountingServiceImpl(Balance balance) {
         this.balance = balance;
     }
 
-//    @Override
-//    public BigDecimal getRow1110() {
-//
-//        if (super.getRow1110() == null)
-//            return BigDecimal.ZERO;
-//        else return super.getRow1110();
-//    }
-//
-//    @Override
-//    public BigDecimal getRow1120() {
-//
-//        if (super.getRow1120() == null)
-//            return BigDecimal.ZERO;
-//        else return super.getRow1120();
-//    }
-
-
-//    public void overr() {
-//        countingService = new CountingServiceImpl(balance);
-//        BigDecimal result =
-//                countingService.getRow1110()
-//                        .add(countingService.getRow1120());
-//        balance.setRow1100(result);
-//    }
-
     public void ruleSumof1100() {
-        BigDecimal result = balance.getRow1110().add(balance.getRow1120()).add(balance.getRow1130().add(balance.getRow1140()
-                .add(balance.getRow1150().add(balance.getRow1160().add(balance.getRow1170()).add(balance.getRow1180()
-                        .add(balance.getRow1190()))))));
+        BigDecimal result = Utils.notNull(balance.getRow1110()).add(Utils.notNull(balance.getRow1120())).add(Utils.notNull(balance.getRow1130())).add(Utils.notNull(balance.getRow1140()))
+                .add(Utils.notNull(balance.getRow1150())).add(Utils.notNull(balance.getRow1160())).add(Utils.notNull(balance.getRow1170())).add(Utils.notNull(balance.getRow1180()))
+                .add(Utils.notNull(balance.getRow1190()));
         balance.setRow1100(result);
     }
 
     public void ruleSumof1200() {
-        BigDecimal result = balance.getRow1210().add(balance.getRow1220()).add(balance.getRow1230().add(balance.getRow1240()
-                .add(balance.getRow1250().add(balance.getRow1260()))));
+        BigDecimal result = Utils.notNull(balance.getRow1210()).add(Utils.notNull(balance.getRow1220())).add(Utils.notNull(balance.getRow1230())).add(Utils.notNull(balance.getRow1240()))
+                .add(Utils.notNull(balance.getRow1250())).add(Utils.notNull(balance.getRow1260()));
         balance.setRow1200(result);
     }
 
     public void ruleSumof1300() {
-        BigDecimal result = balance.getRow1310().subtract(balance.getRow1320()).add(balance.getRow1340()
-                .add(balance.getRow1350().add(balance.getRow1360().add(balance.getRow1370()))));
+        BigDecimal result = Utils.notNull(balance.getRow1310()).subtract(Utils.notNull(balance.getRow1320())).add(Utils.notNull(balance.getRow1340()))
+                .add(Utils.notNull(balance.getRow1350())).add(Utils.notNull(balance.getRow1360())).add(Utils.notNull(balance.getRow1370()));
         balance.setRow1300(result);
     }
 
     public void ruleSumof1400() {
-        BigDecimal result = balance.getRow1410().add(balance.getRow1420()).add(balance.getRow1430()
-                .add(balance.getRow1450()));
+        BigDecimal result = Utils.notNull(balance.getRow1410()).add(Utils.notNull(balance.getRow1420())).add(Utils.notNull(balance.getRow1430()))
+                .add(Utils.notNull(balance.getRow1450()));
         balance.setRow1400(result);
     }
 
     public void ruleSumof1500() {
-        BigDecimal result = balance.getRow1510().add(balance.getRow1520()).add(balance.getRow1530().add(balance.getRow1540()
-                .add(balance.getRow1550())));
+        BigDecimal result = Utils.notNull(balance.getRow1510()).add(Utils.notNull(balance.getRow1520())).add(Utils.notNull(balance.getRow1530())).add(Utils.notNull(balance.getRow1540()))
+                .add(Utils.notNull(balance.getRow1550()));
         balance.setRow1500(result);
     }
 
     public void ruleSumof1600() {
-        BigDecimal result = balance.getRow1100().add(balance.getRow1200());
+        BigDecimal result = Utils.notNull(balance.getRow1100()).add(Utils.notNull(balance.getRow1200()));
         balance.setRow1600(result);
     }
 
     public void ruleSumof1700() {
-        BigDecimal result = balance.getRow1300().add(balance.getRow1400()).add(balance.getRow1500());
+        BigDecimal result = Utils.notNull(balance.getRow1300()).add(Utils.notNull(balance.getRow1400())).add(Utils.notNull(balance.getRow1500()));
         balance.setRow1700(result);
     }
 
     public void ruleSumof1600_1700() throws IncorrectDataException {
-        if (balance.getRow1600() != balance.getRow1700())
+        if (!Utils.notNull(balance.getRow1600()).equals(Utils.notNull(balance.getRow1700())))
             throw new IncorrectDataException(message);
     }
 
-    public void rule1110() throws IncorrectDataException {
-        if (balance.getRow1110().signum() < 0)
-            throw new IncorrectDataException(message);
+    public void ruleMoreThanZero() throws IncorrectDataException
+    {
+        moreThanZero(balance.getRow1110());
+        moreThanZero(balance.getRow1120());
+        moreThanZero(balance.getRow1130());
+        moreThanZero(balance.getRow1140());
+        moreThanZero(balance.getRow1150());
+        moreThanZero(balance.getRow1160());
+        moreThanZero(balance.getRow1170());
 
+        moreThanZero(balance.getRow1190());
+
+        moreThanZero(balance.getRow1200());
+        moreThanZero(balance.getRow1210());
+        moreThanZero(balance.getRow1230());
+        moreThanZero(balance.getRow1240());
+        moreThanZero(balance.getRow1250());
+        moreThanZero(balance.getRow1260());
+
+
+        moreThanZero(balance.getRow1310());
+        moreThanZero(balance.getRow1340());
+        moreThanZero(balance.getRow1350());
+        moreThanZero(balance.getRow1360());
+
+        moreThanZero(balance.getRow1400());
+        moreThanZero(balance.getRow1410());
+        moreThanZero(balance.getRow1430());
+        moreThanZero(balance.getRow1450());
+
+        moreThanZero(balance.getRow1500());
+        moreThanZero(balance.getRow1510());
+        moreThanZero(balance.getRow1520());
+        moreThanZero(balance.getRow1530());
+        moreThanZero(balance.getRow1540());
+        moreThanZero(balance.getRow1550());
+        moreThanZero(balance.getRow1600());
+
+        moreThanZero(balance.getRow1700());
     }
 
+    private void moreThanZero(BigDecimal row) throws IncorrectDataException
+    {
+        if(Utils.notNull(row).signum() < 0)
+            throw new IncorrectDataException("Row can not be less then zero" );
 
-
+    }
 
 }
--- src/test/java/sample/unitTests/BalanceRulesTest.java
+++ src/test/java/sample/unitTests/BalanceRulesTest.java
@@ -12,29 +12,99 @@ import java.math.BigDecimal;
 
 public class BalanceRulesTest {
 
-    IncomeReportRules incomeReportRules = new IncomeReportRules();
-
     @Test
     public void ruleSumof1100() {
-
         Balance balance = new Balance();
         CountingServiceImpl countingService = new CountingServiceImpl(balance);
-        countingService.setRow1110(null);
-        countingService.setRow1120(new BigDecimal(12));
-
-//        countingService.overr();
-        Assert.assertEquals(balance.getRow1100(), new BigDecimal(24));
+        balance.setRow1110(null);
+        balance.setRow1120(new BigDecimal(12));
+        countingService.ruleSumof1100();
+        Assert.assertEquals(new BigDecimal(12), balance.getRow1100());
     }
 
     @Test
-    public void ruleSumof1100Null() {
-
+    public void ruleSumof1200() {
         Balance balance = new Balance();
-//        balance.setRow1110(new BigDecimal(13));
-        balance.setRow1120(new BigDecimal(12));
         CountingServiceImpl countingService = new CountingServiceImpl(balance);
-        countingService.ruleSumof1100();
-        Assert.assertEquals(balance.getRow1100(), new BigDecimal(24));
+        balance.setRow1210(new BigDecimal(14));
+        balance.setRow1220(new BigDecimal(12));
+        countingService.ruleSumof1200();
+        Assert.assertEquals(new BigDecimal(26), balance.getRow1200());
+    }
+
+    @Test
+    public void ruleSumof1300() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1310(new BigDecimal(14));
+        balance.setRow1320(new BigDecimal(12));
+        countingService.ruleSumof1300();
+        Assert.assertEquals(new BigDecimal(2), balance.getRow1300());
+    }
+
+    @Test
+    public void ruleSumof1400() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1410(new BigDecimal(14));
+        balance.setRow1420(new BigDecimal(12));
+        countingService.ruleSumof1400();
+        Assert.assertEquals(new BigDecimal(26), balance.getRow1400());
+    }
+
+    @Test
+    public void ruleSumof1500() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1510(new BigDecimal(14));
+        balance.setRow1520(new BigDecimal(12));
+        countingService.ruleSumof1500();
+        Assert.assertEquals(new BigDecimal(26), balance.getRow1500());
+    }
+
+    @Test
+    public void ruleSumof1600() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1100(new BigDecimal(14));
+        balance.setRow1200(new BigDecimal(12));
+        countingService.ruleSumof1600();
+        Assert.assertEquals(new BigDecimal(26), balance.getRow1600());
+    }
+
+    @Test
+    public void ruleSumof1700() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1300(new BigDecimal(14));
+        balance.setRow1400(new BigDecimal(12));
+        countingService.ruleSumof1700();
+        Assert.assertEquals(new BigDecimal(26), balance.getRow1700());
+    }
+
+    @Test
+    public void ruleSumof1600_1700() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1600(new BigDecimal(14));
+        balance.setRow1700(new BigDecimal(12));
+        try {
+            countingService.ruleSumof1600_1700();
+        } catch (IncorrectDataException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void ruleMoreThanZero() {
+        Balance balance = new Balance();
+        CountingServiceImpl countingService = new CountingServiceImpl(balance);
+        balance.setRow1110(new BigDecimal(-14));
+        try {
+            countingService.ruleMoreThanZero();
+        } catch (IncorrectDataException e) {
+            e.printStackTrace();
+        }
     }
 
 }
